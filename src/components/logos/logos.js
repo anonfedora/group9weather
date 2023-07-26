2a77ba2dd41841138676d91bbb8670304b81e5e2
Musa Eleazar <musaeleazar090@gmail.com>
2023-07-26 02:03:23 +0100

    first commit

---
+++ src/components/logos/logos.js
@@ -0,0 +1,32 @@
+import React from "react";
+
+const Logos = () => {
+  return (
+    <div
+      className="container"
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        paddingTop: "-20px",
+        marginTop: "-50px",
+        marginBottom: "8px",
+      }}
+    >
+      
+      <img
+        alt="logo"
+        className="icon-group-9"
+        src={`icons/SE_logos.png`}
+        style={{
+          height: "100px",
+          width: "100px",
+          marginTop: "15px",
+          marginBottom: "-20px",
+        }}
+      />
+    </div>
+  );
+};
+
+export default Logos;
